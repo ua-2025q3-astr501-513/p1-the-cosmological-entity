7ef2a058a94dcd34bc2f05dc588ce2d3624bdf8e
Alena Casella <alenacasella@dhcp-10-132-211-123.uawifi.arizona.edu>
2025-09-26 16:29:19 -0700

    load files

---
+++ src/calc_pk.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.load("mock_catalog.npz")
+positions = data['pos']
+weights = data['weight']
+
+print(positions[0:10])
+
+data = np.loadtxt("power_spectrum.txt", skiprows=1)
+k = data[:, 0]
+P_k = data[:, 1]
+
+plt.loglog(k,P_k)
+plt.show()
