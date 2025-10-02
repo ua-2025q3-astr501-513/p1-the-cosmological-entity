d23e3fd12e74287c833daf38c8dcc696c40633b9
emacbeth12 <emmimac253@gmail.com>
2025-10-02 16:04:17 -0700

    random comment

--- src/calc_pk.py
+++ src/calc_pk.py
@@ -7,6 +7,7 @@ weights = data['weight']
 
 print(positions[0:10])
 
+# power spectrum used to create mock catalog
 data = np.loadtxt("power_spectrum.txt", skiprows=1)
 k = data[:, 0]
 P_k = data[:, 1]
