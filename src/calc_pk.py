c5775c7295b825b8ce2f6ae3a329e91df4ed6fc2
emacbeth12 <emmimac253@gmail.com>
2025-10-13 22:50:27 -0400

    added path change

--- src/calc_pk.py
+++ src/calc_pk.py
@@ -8,7 +8,7 @@ weights = data['weight']
 print(positions[0:10])
 
 # power spectrum used to create mock catalog
-data = np.loadtxt("power_spectrum.txt", skiprows=1)
+data = np.loadtxt("data/power_spectrum.txt", skiprows=1)
 k = data[:, 0]
 P_k = data[:, 1]
 
